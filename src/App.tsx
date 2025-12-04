c2454f1b250b97df7e255fd680d8a32149c1562f
Oscar-GLS <oscaragutierrezls2002@gmail.com>
2025-12-04 14:52:54 -0500

    Agregar navegación en App y mostrar favoritos en FavoritesPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import { Route, Routes } from "react-router-dom"
+import { Link, Route, Routes } from "react-router-dom"
 import { HomePage, MovieDetailPage, FavoritesPage } from "./pages"
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage/>}/>
-      <Route path="/pelicula/:id" element={<MovieDetailPage/>}/>
-      <Route path="/favoritos" element={<FavoritesPage/>}/>
-    </Routes>
+    <>
+      <nav>
+        <Link to="/">Inicio</Link>
+        <Link to="/favoritos">Ver favoritos</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/pelicula/:id" element={<MovieDetailPage/>}/>
+        <Route path="/favoritos" element={<FavoritesPage/>}/>
+      </Routes>
+    </>
+    
   )
 }
 
--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,5 +1,21 @@
+import { Link } from "react-router-dom";
+import { useFavoritesContext } from "../context/FavoritesContext"
+
 export const FavoritesPage = () => {
+    const {favorites} = useFavoritesContext();
+
     return (
-        <></>
+        <ul>
+            {
+                favorites.length !== 0 && favorites.map((fav)=>(
+                    <li key={fav.id}>
+                        <span>{fav.id}</span>
+                        <span>{fav.title}</span>
+                        <span>{fav.vote_average}</span>
+                        <Link to={`/pelicula/${fav.id}`}>Ver detalles</Link>
+                    </li>
+                ))
+            }
+        </ul>
     )
 }
